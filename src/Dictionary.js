219d0e82432c7f4674e8d594cb73ac03673fb89a
duffy28 <55716177+duffy28@users.noreply.github.com>
2022-08-09 09:01:27 -0400

    Added api
    dictionaryapi.dev

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,24 +1,30 @@
+import axios from "axios";
 import React, { useState } from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [word, setWord] = useState("");
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${word}...`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+    axios.get(apiUrl).then(getData);
   }
-  function wordSearch(event) {
+
+  function getWord(event) {
     console.log(event.target.value);
     setWord(event.target.value);
   }
+
+  function getData(response) {
+    console.log(response.data[0]);
+  }
+
   return (
     <div>
       <form onSubmit={search}>
-        <input
-          type="search"
-          placeholder="Enter a word..."
-          onChange={wordSearch}
-        />
+        <input type="search" placeholder="Enter a word..." onChange={getWord} />
         <input type="submit" value="Search" />
       </form>
     </div>
