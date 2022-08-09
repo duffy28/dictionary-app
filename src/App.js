36390e019743cbfbf18b30afb394a45c78e342a1
duffy28 <55716177+duffy28@users.noreply.github.com>
2022-08-09 07:39:15 -0400

    Added git-link

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <footer>
+        <p>
+          <a
+            href="https://github.com/duffy28/dictionary-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-Source Code
+          </a>{" "}
+          by Shaun Duffy
+        </p>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
