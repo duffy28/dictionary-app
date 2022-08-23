e6b724cb758e12a5123aa8451d43bfd800d5f974
duffy28 <55716177+duffy28@users.noreply.github.com>
2022-08-23 11:52:07 -0400

    Update Photos.js

--- src/Photos.js
+++ src/Photos.js
@@ -5,23 +5,21 @@ import "./Photos.css";
 export default function Photos(props) {
   if (props.data) {
     return (
-      <div className="Photos container ">
-        <div className="text-box">
-          <div className="row">
-            {props.data.map(function (photo, index) {
-              return (
-                <div className="col-4" key={index}>
-                  <a href={photo.src.original} target="_blank" rel="noreferrer">
-                    <img
-                      src={photo.src.landscape}
-                      className="img-fluid"
-                      alt="Sample of dictionary result"
-                    />
-                  </a>
-                </div>
-              );
-            })}
-          </div>
+      <div className="Photos container text-box">
+        <div className="row">
+          {props.data.map(function (photo, index) {
+            return (
+              <div className="col-4" key={index}>
+                <a href={photo.src.original} target="_blank" rel="noreferrer">
+                  <img
+                    src={photo.src.landscape}
+                    className="img-fluid"
+                    alt="Sample of dictionary result"
+                  />
+                </a>
+              </div>
+            );
+          })}
         </div>
       </div>
     );
